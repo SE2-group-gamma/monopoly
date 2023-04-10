a356fe184c500d4814bd479f14f172cf27c069c5
Martin Jäger <martinja@edu.aau.at>
2023-04-10 17:22:16 +0200

    Implemented the Board, Field and Player Objects

--- app/src/main/java/com/example/monopoly/Field.java
+++ app/src/main/java/com/example/monopoly/Field.java
@@ -5,18 +5,79 @@ import android.graphics.Color;
 public class Field {
 
     private int id;
-    private String type;
+    private String name;
+    private String type;        //Type of Field
     private Color col;          //Fields with approximately the same price have the same color
-    private int cost;
+    private int cost;           //Field Cost
+    private int houses;         //Houses on Field
     private Player owner;       //Gets set if bought for the first time
+    private int houseCost;      //Price per house
+    private int mortgageValue;
+    private int mortgageCancellationValue;
 
-    public Field(int id, String type, Color col, int cost) {
+    public Field(int id, String name, String type, Color col, int cost, int houses, int houseCost, int mortgageValue, int mortgageCancellationValue) {
         this.id = id;
+        this.name = name;
         this.type = type;
         this.col = col;
         this.cost = cost;
+        this.houses = houses;
+        this.houseCost = houseCost;
+        this.mortgageValue = mortgageValue;
+        this.mortgageCancellationValue = mortgageCancellationValue;
     }
 
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setHouses(int houses) {
+        this.houses = houses;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public int getHouseCost() {
+        return houseCost;
+    }
+
+    public int getMortgageValue() {
+        return mortgageValue;
+    }
+
+    public int getMortgageCancellationValue() {
+        return mortgageCancellationValue;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Color getCol() {
+        return col;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getHouses() {
+        return houses;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
 }
 
 
--- app/src/main/java/com/example/monopoly/Player.java
+++ app/src/main/java/com/example/monopoly/Player.java
@@ -1,11 +1,19 @@
 package com.example.monopoly;
 
+import android.graphics.Color;
+
 public class Player {
     private String username;
+    private Color col;
     private double capital;
+    private boolean alive;
+    private boolean inPrison;
 
-    public Player(String username, double capital) {
+    public Player(String username, Color col, double capital, boolean alive) {
         this.username = username;
+        this.col = col;
         this.capital = capital;
+        this.alive = true;
+        this.inPrison = false;
     }
 }
