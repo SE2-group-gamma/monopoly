b2690c754aa1a4f0dccee7bbfd76a2a1f274664f
Clemi2806 <38780028+Clemi2806@users.noreply.github.com>
2023-04-13 21:53:31 +0200

    Update Gradle Test-Config
    Update the gradle config, with the following changes:
    - newer Java version
    - newer JUnit version
    
    Additionally remove template Unit-Tests

--- app/src/test/java/com/example/monopoly/MonopolyServerTest.java
+++ app/src/test/java/com/example/monopoly/MonopolyServerTest.java
@@ -1,15 +1,14 @@
 package com.example.monopoly;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import com.example.monopoly.network.MonopolyServer;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.ConnectException;
