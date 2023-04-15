4041d0cecdb3a479ab2353fea8d32c10776684c7
Martin Jäger <martinja@edu.aau.at>
2023-04-15 23:27:18 +0200

    Completed Tests for the Field-object

---
+++ app/src/test/java/com/example/monopoly/FieldTest.java
@@ -0,0 +1,110 @@
+package com.example.monopoly;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import android.graphics.Color;
+import android.media.Image;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.Mock;
+
+class FieldTest {
+    static Field field;
+    static Player owner;
+    static Image img;
+    static Color col;
+
+    @BeforeEach
+    void setup(){
+        img = mock(Image.class);
+        owner = mock(Player.class);
+        col = mock(Color.class);
+        field = new Field(0,"Billa","Ereignis",col,100,3,owner,50000,33000,30000,img);
+    }
+    @Test
+    void setCost() {
+        assertEquals(field.getCost(),100);
+        field.setCost(10);
+        assertEquals(field.getCost(),10);
+    }
+
+    @Test
+    void setHouses() {
+        assertEquals(field.getHouses(),3);
+        field.setHouses(10);
+        assertEquals(field.getHouses(),10);
+    }
+
+    @Test
+    void setGetOwner() {
+        assertEquals(field.getOwner(), owner);
+        Player newOwner = mock(Player.class);
+        field.setOwner(newOwner);
+        assertEquals(field.getOwner(), newOwner);
+    }
+
+    @Test
+    void getHouseCost() throws NoSuchFieldException {
+        assertEquals(field.getHouseCost(), 50000);
+    }
+
+    @Test
+    void getMortgageValue() {
+        assertEquals(field.getMortgageValue(),33000);
+    }
+
+    @Test
+    void getMortgageCancellationValue() {
+        assertEquals(field.getMortgageCancellationValue(),30000);
+    }
+
+    @Test
+    void getId() {
+        assertEquals(field.getId(),0);
+    }
+
+    @Test
+    void getName() {
+        assertEquals(field.getName(),"Billa");
+    }
+
+    @Test
+    void getType() {
+        assertEquals(field.getType(), "Ereignis");
+    }
+
+    @Test
+    void getCol() {
+        assertEquals(field.getCol(),col);
+    }
+
+    @Test
+    void getCost() {
+        assertEquals(field.getCost(),100);
+        field.setCost(120);
+        assertEquals(field.getCost(),120);
+    }
+
+    @Test
+    void getHouses() {
+        assertEquals(field.getHouses(),3);
+        field.setHouses(5);
+        assertEquals(field.getHouses(),5);
+    }
+
+}
