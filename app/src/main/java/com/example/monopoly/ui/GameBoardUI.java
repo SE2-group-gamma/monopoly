999ce8be5f9ea0518997eba8311185d01a6d7f43
Martin Jäger <martinja@edu.aau.at>
2023-04-22 18:49:41 +0200

    Created GameBoardUI including a button and the image

--- app/src/main/java/com/example/monopoly/ui/GameBoardUI.java
+++ app/src/main/java/com/example/monopoly/ui/GameBoardUI.java
@@ -1,4 +1,51 @@
 package com.example.monopoly.ui;
 
-public class GameBoardUI {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.monopoly.R;
+import com.example.monopoly.databinding.GameBoardBinding;
+
+public class GameBoardUI extends Fragment {
+
+    private GameBoardBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = GameBoardBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Log.d("Test",binding.button.getText().toString());
+        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // TODO: Set textView with key from Lobby
+                //binding.textViewKey.setText("Game-Key: "+key);
+            }
+        });
+
+        binding.backButton.setOnClickListener(view1 -> NavHostFragment.findNavController(GameBoardUI.this)
+                .navigate(R.id.action_GameBoard_to_FirstFragment));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
