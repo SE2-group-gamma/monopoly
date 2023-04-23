ffac440636662556f9d0bbd9a7edd3cc77c975fe
Clemi2806 <38780028+Clemi2806@users.noreply.github.com>
2023-04-23 17:29:34 +0200

    Added Dices class
    Added Dices class to represent dices in the game. Also added
    testcases for the public methods.

---
+++ app/src/main/java/com/example/monopoly/gamelogic/Dices.java
@@ -0,0 +1,47 @@
+package com.example.monopoly.gamelogic;
+
+import java.util.Random;
+
+public class Dices {
+    private int dice1, dice2;
+    private boolean isLastRollFlawed;
+
+    private Random random;
+
+    public Dices() {
+        super();
+        this.dice1 = 0;
+        this.dice2 = 0;
+        this.isLastRollFlawed = false;
+        random = new Random();
+    }
+
+    public void rollDices(){
+        this.isLastRollFlawed = false;
+        this.dice1 = random.nextInt(6)+1;
+        this.dice2 = random.nextInt(6)+1;
+    }
+
+    public void rollDicesFlawed(int sum) throws IllegalArgumentException{
+        if(sum < 2 || sum > 12) throw new IllegalArgumentException("Invalid sum of two dices: " + sum);
+        this.isLastRollFlawed = true;
+        this.dice1 = random.nextInt(6)+1;
+        this.dice2 = sum - this.dice1;
+    }
+
+    public int getDice1() {
+        return dice1;
+    }
+
+    public int getDice2() {
+        return dice2;
+    }
+
+    public boolean isLastRollFlawed() {
+        return isLastRollFlawed;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+}
---
+++ app/src/test/java/com/example/monopoly/DicesTest.java
@@ -0,0 +1,70 @@
+package com.example.monopoly;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.example.monopoly.gamelogic.Dices;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.Random;
+
+public class DicesTest {
+
+    static Random random;
+
+    @BeforeAll
+    public static void initRandom(){
+        random = new Random();
+    }
+
+    @Test
+    public void testCreateDices(){
+        Dices dices = new Dices();
+        assertNotNull(dices);
+    }
+
+    @Test
+    public void testDiceRollFlawed(){
+        Dices dices = new Dices();
+
+        for(int i = 0; i < 10; i++){
+            int sum = random.nextInt(11)+2;
+            dices.rollDicesFlawed(sum);
+            assertEquals(sum, dices.getDice1()+ dices.getDice2());
+        }
+    }
+
+    @Test
+    public void testDiceRoll(){
+        Dices dices = new Dices();
+        Random r = mock(Random.class);
+        when(r.nextInt(anyInt())).then(new Answer<Integer>() {
+            int count = 0;
+            @Override
+            public Integer answer(InvocationOnMock invocation) throws Throwable {
+                return count++ == 0 ? 2 : 3;
+            }
+        });
+        dices.setRandom(r);
+        dices.rollDices();
+        assertFalse(dices.isLastRollFlawed());
+        assertEquals(3, dices.getDice1());
+        assertEquals(4, dices.getDice2());
+    }
+
+    @Test
+    public void invalidSum(){
+        Dices dices = new Dices();
+        assertThrows(IllegalArgumentException.class, () -> {dices.rollDicesFlawed(100);});
+    }
+}
