ec6e8b81a5dff6da93dccf47fcb6714ded1459fe
Clemi2806 <38780028+Clemi2806@users.noreply.github.com>
2023-04-23 18:01:07 +0200

    Add use-feature for accelerometer to project
    Add the use-feature element for the accelerometer to the projects
    AndroidManifest.xml

---
+++ app/src/main/java/com/example/monopoly/ui/DiceFragment.java
@@ -0,0 +1,66 @@
+package com.example.monopoly.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.monopoly.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DiceFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DiceFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DiceFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DiceFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DiceFragment newInstance(String param1, String param2) {
+        DiceFragment fragment = new DiceFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dice, container, false);
+    }
+}
