0d0a86bced3d2480c8f34f5717b84f112ef93ce2
Cathrin <cathrin_s@hotmail.com>
2023-05-06 13:23:56 +0200

    Added methods createCards(), addToAllCommunityChestCards() and addCardsToDeck() to CommunityChestCardCollection and Testcases

--- app/src/test/java/com/example/monopoly/CommunityChestCardTest.java
+++ app/src/test/java/com/example/monopoly/CommunityChestCardTest.java
@@ -1,11 +1,15 @@
 package com.example.monopoly;
 
+import com.example.monopoly.gamelogic.ChanceCard;
 import com.example.monopoly.gamelogic.CommunityChestCard;
+import com.example.monopoly.gamelogic.CommunityChestCardCollection;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CommunityChestCardTest {
+    CommunityChestCardCollection collection;
 
     @Test
     public void createCommunityChestTest(){
@@ -13,6 +17,32 @@ public class CommunityChestCardTest {
         card.setFunction("test");
         Assertions.assertEquals(0,card.getId());
         Assertions.assertEquals("test",card.getFunction());
+    }
+
+    @BeforeEach
+    public void createCardsAndFill(){
+        collection = new CommunityChestCardCollection();
+    }
+    @Test
+    public void createCardsTest(){
+        Assertions.assertEquals(20, collection.getAllCommunityChestCards().size());
+        Assertions.assertEquals(20, collection.getCommunityChestCardDeck().size());
+    }
+
+    @Test
+    public void printAllCards() {
+        for (int i = 0; i < collection.getAllCommunityChestCards().size(); i++) {
+            CommunityChestCard card = (CommunityChestCard) collection.getAllCommunityChestCards().get(i);
+            System.out.println(card.getId() + ", " + card.getFunction());
+        }
+    }
+
+    @Test
+    public void printDeck() {
+        for (int i = 0; i < collection.getCommunityChestCardDeck().size(); i++) {
+            CommunityChestCard card = (CommunityChestCard) collection.getCommunityChestCardDeck().get(i);
+            System.out.println(card.getId() + ", " + card.getFunction());
+        }
     }
 
 }
