bba3a47660d27088e5023a59731b8a352c06909e
Cathrin <cathrin_s@hotmail.com>
2023-05-06 12:43:36 +0200

    Added Attributes, Constuctor and Getters/Setters in CommunityChestCard

--- app/src/main/java/com/example/monopoly/gamelogic/CommunityChestCard.java
+++ app/src/main/java/com/example/monopoly/gamelogic/CommunityChestCard.java
@@ -1,4 +1,28 @@
 package com.example.monopoly.gamelogic;
 
 public class CommunityChestCard {
+    private int id;
+    private String function;
+
+    public CommunityChestCard(int id) {
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getFunction(){
+        return function;
+    }
+
+    public void setFunction(String function){
+        this.function = function;
+    }
+
+
 }
