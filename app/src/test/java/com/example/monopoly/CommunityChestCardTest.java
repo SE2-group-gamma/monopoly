c2f00e121fbc8a13f2e0220dbebc1e82b78ad3ca
Cathrin <cathrin_s@hotmail.com>
2023-05-06 12:48:45 +0200

    Added Class CommunityChestCardTest and first Testcase

---
+++ app/src/test/java/com/example/monopoly/CommunityChestCardTest.java
@@ -0,0 +1,17 @@
+package com.example.monopoly;
+
+import com.example.monopoly.gamelogic.CommunityChestCard;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CommunityChestCardTest {
+
+    @Test
+    public void createCommunityChestTest(){
+        CommunityChestCard card = new CommunityChestCard(0);
+        card.setFunction("test");
+        Assertions.assertEquals(0,card.getId());
+        Assertions.assertEquals("test",card.getFunction());
+    }
+}
