8f2a1799b3351acfecd2da03b5714b8ae8ba69ee
Martin Jäger <martinja@edu.aau.at>
2023-05-07 11:48:02 +0200

    Added Game testclass with tests

---
+++ app/src/test/java/com/example/monopoly/GameTest.java
@@ -0,0 +1,34 @@
+package com.example.monopoly;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import android.graphics.Color;
+
+import com.example.monopoly.gamelogic.Game;
+import com.example.monopoly.gamelogic.Player;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    Game g;
+    Player p;
+
+    @BeforeEach
+    void setup(){
+        g = new Game();
+        p = new Player("Test",new Color(),100.00,true);
+    }
+
+    @Test
+    public void testAddPlayer(){
+        assertEquals(0,g.getPlayers().size());
+        assertEquals(true,g.addPlayer(p));
+        assertEquals(p,g.getPlayers().get(1));
+        assertEquals(1,g.getPlayers().size());
+        assertEquals(false,g.addPlayer(p));
+        assertEquals(1,g.getPlayers().size());
+    }
+}
