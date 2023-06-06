0652a7288dff226973273956d246cd0715807000
Martin Jäger <martinja@edu.aau.at>
2023-06-06 18:40:08 +0200

    Removed dead code

--- app/src/main/java/com/example/monopoly/ui/viewmodels/GameBoardUIViewModel.java
+++ app/src/main/java/com/example/monopoly/ui/viewmodels/GameBoardUIViewModel.java
@@ -11,12 +11,6 @@ public class GameBoardUIViewModel extends ViewModel {
     private final MutableLiveData<Boolean> uncover = new MutableLiveData<>();
     private final MutableLiveData<Boolean> throwDice = new MutableLiveData<>();
 
-    /*public void setClient(Client client) {
-        this.client.setValue(client);
-    }
-    public LiveData<Client> getClientData() {
-        return this.client;
-    }*/
     public void setUncoverEnabled(boolean enabled){
         this.uncover.setValue(enabled);
     }
