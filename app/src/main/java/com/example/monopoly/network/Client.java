904efbb76f28950272a3b5554c81f613152fa3b6
DavidMichaelStark <david.stark.travar@gmail.com>
2023-06-16 15:42:34 +0200

    Deleted redundant lines of code and set gradle to standard values

--- app/src/main/java/com/example/monopoly/network/Client.java
+++ app/src/main/java/com/example/monopoly/network/Client.java
@@ -238,9 +238,6 @@ public class Client extends Thread {
                 if (turnEnd) {
                     turnProcess();
                 }
-                /*if (gameStart == true) {
-                    setRanks(HostGame.getPlayerCount());
-                }*/
             }
 
         } catch(IOException io) {
